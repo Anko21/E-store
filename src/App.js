f247bbdc3dbd6c92a9ca28101092d1b986f9e59b
Anko2136 <am.kontaratou@gmail.com>
2024-10-25 11:04:58 +0100

    dropdown selection added in searchbar

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ function App() {
             <h1>eStore</h1>
           </div>
           <div className='inp-container p-0 my-4 w-50 h-25 bg-white'>
+            <div className='dropdown m-0 p=0'>
+              <select className='select-btn w-100 p-0 m-0'>
+                <option>Men</option>
+                <option>Women</option>
+                <option>Kids</option>
+              </select>
+            </div>
             <input className= 'form control' placeholder='Search'/>
             <button><i className='fa fa-search'/></button>
           </div>
